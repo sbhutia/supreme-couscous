67e9b8815c27f6c7afc44cb0915e1ae561e2d44f
Wang <sbhutia13@gmail.com>
2016-08-20 23:21:28 +0100

    added files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,24 @@
 module.exports = function(grunt){
     const path = require('path');
-    
+
     grunt.initConfig({
+        copy: {
+              main: {
+                files: [
+                  // includes files within path
+                  {expand: true, src: ['bower_components/jquery/dist/jquery.js'], dest: 'web/js/', flatten: true}
+
+                  /*// includes files within path and its sub-directories
+                  {expand: true, src: ['path/**'], dest: 'dest/'},
+
+                  // makes all src relative to cwd
+                  {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+
+                  // flattens results to a single level
+                  {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},*/
+                ],
+              },
+            },
       express: {
         server: {
           options: {
@@ -12,6 +29,7 @@ module.exports = function(grunt){
       }
     });
     grunt.loadNpmTasks('grunt-express');
-    
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
     grunt.registerTask('default',['express', 'express-keepalive'])
 };
